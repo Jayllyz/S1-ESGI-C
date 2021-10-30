05bbb3a5a1a4cc363e1f649d2ca31b738762f45a
Opixelum <12w0zvkgazd3@opayq.net>
2021-10-30 20:43:20 +0200

    Ajout du menu (sans le code du pattern) (à résoudre)

--- ex1.c
+++ ex1.c
@@ -1,70 +1,52 @@
 /*
-    Objectif : Dessiner avec des "*" une forme variable en taille.
-    Date : 30/10/2021
-
+    Objectif : Afficher un pattern de '*' avec un nombre n de lignes.
+    Autheurs : Anto BENEDETTI, Antony DAVID, Anthony JABRE
+    Date     : 30/10/2021
 */
+
 #include <stdio.h>
-#define n 6
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
+    char n;       // Nombre de lignes constituant le pattern.
+    char i, j, k; // Variable pour les boucles.
+    char running = 1; //1: le programme est répété, 0: le programme s'arrête. 
 
-int i,j,k=0;
+    do {
+        printf("Exercice 1 :\n1. Lancer\n2. Quitter\nEntrez votre choix: ");
+        fflush(stdin);
+        scanf("%d", &n); // La variable n sert également à stocker le choix de l'utilisateur
 
-for(i=0;i<n+1;i++){
-    for(j=0;j<=i;j++){
-        printf("*");
-    }
-    for(j=i;j<=n;j++){
-        printf(" ");
-    }
-    for(k=i;k<n;k++){
-        printf(" ");
-    }
-    for(j=0;j<=i;j++){
-        printf("*");
-    }
-    printf("\n");
-}
+        switch (n) {
+            // Lance le programme.
+            case 1:
+                printf("*running*\n");
+                break;
+            
+            // Arrête le programme.
+            case 2:
+                running = 0;
+                break;
 
-for(k=0;k<n*2+3;k++)//ligne centrale (maybe autre technique)
-    printf("*");
-
-for(i=0;i<n+1;i++){
-    printf("\n");
-    for(j=i;j<=n;j++){
-        printf("*");
-    }
-    for(j=0;j<=i;j++){
-            printf(" ");
-    }
-    for(j=0;j<i;j++){
-            printf(" ");
-    }
-    for(j=i;j<=n;j++){
-        printf("*");
-    }
-}
-printf("\n");
-
-return 0;
+            // Mauvaise valeur tapée par l'utilisateur.
+            default:
+                printf("Mauvaise entrée. Taper un chiffre uniquement entre 1 et 2.\n\n");
+                break;
+        }
+    } while (running);
+    
+    return 0;
 }
 
 /*
 
-*             *
-**           **
-***         ***
-****       ****
-*****     *****
-******   ******
-******* *******
-***************
-******* *******
-******   ******
-*****     *****
-****       ****
-***         ***
-**           **
-*             *
+    *               * 
+    * *           * * 
+    * * *       * * * 
+    * * * *   * * * * 
+    * * * * * * * * * 
+    * * * *   * * * * 
+    * * *       * * * 
+    * *           * * 
+    *               * 
 
 */
