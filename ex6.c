1a7bd86d368c1b4ba3b0e47fa5108297ed09f52d
Jayllyz <antonydavid945@gmail.com>
2021-11-14 14:06:29 +0100

    first update

--- ex6.c
+++ ex6.c
@@ -2,33 +2,39 @@
 //./EXEC
 #include "ez-draw.h"
 
+void win1_on_expose (Ez_event *ev)
+{
+    int  w, h;
+    ez_window_get_size (ev->win, &w, &h);
+    ez_set_color (ez_black);
+    ez_draw_text (ev->win, EZ_TC, w/2, 1, "q pour quitter");
+    ez_draw_line (ev->win,  50,  50,50,h-50);
+    ez_draw_line (ev->win, 50,  h/2,w-50,h/2);
+    ez_draw_text (ev->win, EZ_TC, 35, 50, "X");
+    ez_draw_text (ev->win, EZ_TC, w-50, h/2 + 10, "Y");
+    
+}
 
-void win1_on_event (Ez_event *ev)                /* Called by ez_main_loop() */
-{                                                /* for each event on win1   */
+void win1_on_key_press (Ez_event *ev)
+{
+    switch (ev->key_sym) {
+        case XK_q : ez_quit (); break;
+    }
+
+}
+
+void win1_on_event (Ez_event *ev)//main function
+{
     switch (ev->type) {
-
-        case Expose :                           /* We must redraw everything */
-            ez_set_color (ez_red);
-            ez_draw_text (ev->win, EZ_MC, 200, 150, 
-                "To quit, press the key 'q', or click\n"
-                "on the Close button of the window");
-            break;
-
-        case KeyPress :                                 /* A key was pressed */
-            switch (ev->key_sym) {
-                case XK_q : ez_quit (); break;
-            }
-            break;
+        case Expose   : win1_on_expose    (ev); break;
+        case KeyPress : win1_on_key_press (ev); break;
     }
 }
 
-
-int main(int argc, char **argv)
+int main()
 {
     if (ez_init() < 0) exit(1);
-
-    ez_window_create (400, 300, "EXO 6", win1_on_event);
-
+        ez_window_create (800, 800, "Exercice 6", win1_on_event);
     ez_main_loop ();
     exit(0);
 }
