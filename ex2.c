fb6035a3b97c441795871b3da7aef5b8bce0550f
Opixelum <12w0zvkgazd3@opayq.net>
2021-11-14 16:00:06 +0100

    Code improvements
    Meilleure affichage du résultat (presque terminé)
    Rennommage de variables
    Vérification de l'input terminée

--- ex2.c
+++ ex2.c
@@ -13,8 +13,8 @@ int main(int argc, char **arg)
     unsigned char running = 1; // 0 : arrêt du programme, 1 : execution du programme.
     int inpt; // Valeur saisie par l'utilisateur (choix du menu, chiffre/nombre pour le programme).
     unsigned char type_check; // 1 : scanf a bien récupéré un nombre, et non un autre type de valeur.
-    unsigned char inpt_check; // Doit être égal à 2 pour que la valeur saisie par l'utilisateur soit conforme.
-    unsigned char digits_sum; // Somme des deux chiffres composant le nombre
+    unsigned char inpt_check; // Doit être égal à 3 pour que la valeur saisie par l'utilisateur soit conforme.
+    unsigned char bars; // Barres nécessaires pour un chiffre
     unsigned char is_good; // Egal à 1 si le nombre est magique.
     
 
@@ -22,9 +22,10 @@ int main(int argc, char **arg)
     do
     {
         printf("\nExercice 2 :\n1. Lancer\n2. Quitter\nEntrez votre choix : ");
+        fflush(stdin); // Purge du buffer pour éviter tout conflit avec les futurs scanf. Valable pour tous les fflush.
         type_check = scanf("%d", &inpt);
-
-
+        
+        
         // Vérification de la saisie de "inpt" pour le menu
         while (inpt_check != 3)
         {
@@ -34,7 +35,7 @@ int main(int argc, char **arg)
             if (type_check != 1)
             {
                 printf("ERREUR : Vous avez saisi un caract%cre.\nVeuillez saisir soit '1', soit '2' : ", 138);
-                fflush(stdin); // Purge du buffer nécessaire uniquement lorsque de la saisie de caractères
+                fflush(stdin);
                 type_check = scanf("%d", &inpt);
             }
             else
@@ -44,15 +45,17 @@ int main(int argc, char **arg)
             if (inpt < 0)
             {
                 printf("ERREUR : Vous avez saisi une valeur trop petite.\nVeuillez saisir soit '1', soit '2' : ");
+                fflush(stdin);
                 type_check = scanf("%d", &inpt);
             }
             else
                 inpt_check++;
 
             // Si la valeur est trop grande
-            if (inpt > 9)
+            if (inpt > 2)
             {
                 printf("ERREUR : Vous avez saisi une valeur trop grande.\nVeuillez saisir soit '1', soit '2' : ");
+                fflush(stdin);
                 type_check = scanf("%d", &inpt);
             }
             else
@@ -66,6 +69,7 @@ int main(int argc, char **arg)
             // 1. Lancer
             case 1:
                 printf("\nVeuillez saisir un nombre compris entre 0 et 99 (inclus) : ");
+                fflush(stdin);
                 type_check = scanf("%d", &inpt);
 
                 // Vérification de la saisie de "inpt" pour le chiffre/nombre magique
@@ -87,6 +91,7 @@ int main(int argc, char **arg)
                     if (inpt < 0)
                     {
                         printf("ERREUR : Vous avez saisi une valeur n%cgative.\nVeuillez saisir un nombre compris entre 0 et 99 (inclus) : ", 130);
+                fflush(stdin);
                         type_check = scanf("%d", &inpt);
                     }
                     else
@@ -96,6 +101,7 @@ int main(int argc, char **arg)
                     if (inpt > 99)
                     {
                         printf("ERREUR : Vous avez saisi une valeur trop grande.\nVeuillez saisir un nombre compris entre 0 et 99 (inclus) : ");
+                fflush(stdin);
                         type_check = scanf("%d", &inpt);
                     }
                     else
@@ -109,67 +115,82 @@ int main(int argc, char **arg)
                 {
                     unsigned char digit1 = inpt / 10; // Premier chiffre du nombre
                     unsigned char digit2 = inpt % 10; // Deuxième chiffre du nombre
-                    unsigned char test_inpt = digit1;
-                    unsigned char bars = 0; // Barres nécessaires pour afficher la valeur
-                    printf("\nPremier chiffre : %d\nDeuxi%cme chiffre : %d\nNombre de base : %d\n", digit1, 138, digit2, inpt);
-                    digits_sum = digit1 + digit2;
-                    printf("%d + %d = %d\n", digit1, digit2, digits_sum);
 
-                    // Boucle de calcul de l'addition des barres des chiffre du nombre
-                    for (int i = 0; i < 2; ++i)
+                    unsigned char test_digit = digit1; // Sélection du chiffre à traiter
+                    unsigned char bars_sum = 0; // Somme des barres des deux chiffres
+
+                    // Boucle pour l'addition des barres des chiffres
+                    for (int i = 1; i <= 2; ++i)
                     {
-                        if (i == 1)
-                            test_inpt = digit2;
+                        // Changement du chiffre à traiter
+                        if (i == 2)
+                            test_digit = digit2;
 
-                        switch (test_inpt)
+                        switch (test_digit)
                         {
                             case 0:
-                                bars += 6;
+                                bars = 6;
+                                bars_sum += bars;
                                 break;
                             case 1:
-                                bars += 2;
+                                bars = 2;
+                                bars_sum += bars;
                                 break;
 
                             case 2:
-                                bars += 5;
+                                bars = 5;
+                                bars_sum += bars;
                                 break;
 
                             case 3:
-                                bars += 5;
+                                bars = 5;
+                                bars_sum += bars;
                                 break;
 
                             case 4:
-                                bars += 4;
+                                bars = 4;
+                                bars_sum += bars;
                                 break;
                             
                             case 5:
-                                bars += 5;
+                                bars = 5;
+                                bars_sum += bars;
                                 break;
                             
                             case 6:
-                                bars += 6;
+                                bars = 6;
+                                bars_sum += bars;
                                 break;
 
                             case 7:
-                                bars += 3;
+                                bars = 3;
+                                bars_sum += bars;
                                 break;
 
                             case 8:
-                                bars += 7;
+                                bars = 7;
+                                bars_sum += bars;
                                 break;
 
                             case 9:
-                                bars += 6;
+                                bars = 6;
+                                bars_sum += bars;
                                 break;
                         }
+                        if (i == 1)
+                            printf("\nPremier chiffre    : %d --> %d barres", digit1, bars);
+                        
+                        else 
+                            printf("\nDeuxi%cme chiffre   : %d --> %d barres", 138, digit2, bars);
                     }
+                    printf("\nSomme des barres   : %d", bars_sum);
+                    printf("\nSomme des chiffres : %d\n", digit1 + digit2);
+
                     // Si le nombre de barres des 2 chiffres composant le nombre
                     // et la somme des 2 chiffres sont égaux,
                     // on affecte la valeur 1 à "is_good" afin d'indiquer que c'est un nombre magique.
-                    if (bars == digits_sum)
+                    if (bars_sum == digit1 + digit2)
                         is_good = 1;
-                    else
-                        printf("%d != %d\n", digits_sum, bars);
                 }
 
                 // Cas le plus simple où l'on nous donne 1 seul chiffre
@@ -184,13 +205,13 @@ int main(int argc, char **arg)
 
                 // Test final pour voir si c'est un chiffre/nombre magique
                 if (is_good && inpt < 10)
-                    printf("\nLe chiffre est magique.\n");
+                    printf("\nLe chiffre est magique (chiffre = barres n%ccessaires).\n", 130);
                 else if (is_good && inpt >= 10)
-                    printf("\nLe nombre est magique.\n");
+                    printf("\nLe nombre est magique (somme des chiffres = somme des barres).\n");
                 else if (inpt < 10)
-                    printf("\nLe chiffre n'est pas magique.\n");
+                    printf("\nLe chiffre n'est pas magique (chiffre != barres n%ccessaires).\n", 130);
                 else
-                    printf("\nLe nombre n'est pas magique.\n");
+                    printf("\nLe nombre n'est pas magique (somme des chiffres != somme des barres).\n");
                 
                 break;
 
