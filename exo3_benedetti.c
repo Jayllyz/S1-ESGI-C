594b065c1c1476051ddca24a6e0fda752620419c
Opixelum <12w0zvkgazd3@opayq.net>
2021-11-27 14:45:13 +0100

    Refacto (2 versions du code possible)

--- exo3_benedetti.c
+++ exo3_benedetti.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv){
     unsigned char check_step; // Nombre d'étapes de vérification validées
 
     // Variables relatives à l'exercice
+    int i, j; // Variables pour boucles
     // Coordonnées de base
     int xa = 0;
     int ya = 0;
@@ -31,8 +32,8 @@ int main(int argc, char **argv){
     int find_x = 0;
     int find_y = 0;
 
-
-    do {
+    do
+    {
         printf("\nExercice 3 :\n1. Lancer\n2. Quitter\nSaisissez votre choix : ");
         fflush(stdin); // Purge du buffer d'entrées pour éviter tout conflit. Valable pour tous
         type_check = scanf("%d", &usr_inpt);
@@ -99,7 +100,7 @@ int main(int argc, char **argv){
                         check_step++;
 
                     // Si la valeur est trop petite
-                    if (xa < 0 || ya < 0)
+                    if (xa <= 0 || ya <= 0)
                     {
                         printf("ERREUR : Vous avez saisi une valeur trop petite.\nVeuillez r%cessayer : ", 130);
                         fflush(stdin);
@@ -208,75 +209,245 @@ int main(int argc, char **argv){
                 int width = xb - xa;
                 int height = yb - ya;
 
-                // Boucle principal parcourant les zones afin de calculer les coord des zones et vérifier si les coord sont dedans
+                // <---------------------- Z O N E S ------------------------>
+                // Création des zones selon les points 'a' et 'b'.
                 for (int i = 1; i <= 9; ++i)
                 {
+                    printf("\n=============================================\nZone %d\n", i);
+                    
+                    // Chaque "case" correspond à une zone
                     switch (i)
                     {
                         case 1 :
-                            printf("=============================================\nZone 1\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (0, 0)\n", 130, 130);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xa, ya);
                             break;
 
                         case 2 :
-                            printf("=============================================\nZone 2\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (%d, 0)\n", 130, 130, xa);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xb, ya);
                             break;
 
                         case 3 :
-                            printf("=============================================\nZone 3\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (%d, 0)\n", 130, 130, xb);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xb + width, ya);
                             break;
 
                         case 4 :
-                            printf("=============================================\nZone 4\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (0, %d)\n", 130, 130, ya);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xa, yb);
                             break;
 
                         case 5 :
-                            printf("=============================================\nZone 5\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (%d, %d)\n", 130, 130, xa, ya);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xb, yb);
                             break;
 
                         case 6 :
-                            printf("=============================================\nZone 6\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (%d, %d)\n", 130, 130, xb, ya);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xb + width, yb);
                             break;
 
                         case 7 :
-                            printf("=============================================\nZone 7\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (0, %d)\n", 130, 130, yb);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xa, yb + height);
                             break;
 
                         case 8 :
-                            printf("=============================================\nZone 8\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (%d, %d)\n", 130, 130, xa, yb);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xb, yb + height);
                             break;
 
                         case 9 :
-                            printf("=============================================\nZone 9\n");
                             printf("Coordonn%ces du coin sup%crieur gauche : (%d, %d)\n", 130, 130, xb, yb);
                             printf("Coordonn%ces du coin inf%crieur droit  : (%d, %d)\n", 130, 130, xb + width, yb + height);
                             printf("=============================================\n");
                             break;
                     }
                 }
+
+                // <---------- R E C H E R C H E   D U   P O I N T ---------->
+                printf("\nLe point recherch%c se trouve", 130);
+
+                // Frontière colonnes 1/extérieur
+                if (find_x == 0)
+                {
+                    if (find_y == 0)
+                        printf(" au coin sup%crieur gauche de la zone 1.\n", 130);
+                    
+                    else if (find_y < ya)
+                        printf(" sur la fronti%cre gauche ex%crieur/zone 1.\n", 138, 130);
+
+                    else if (find_y == ya)
+                        printf(" %c l'intersection des zones 1, 4 & l'ext%crieur.\n", 133, 130);
+
+                    else if (find_y < yb)
+                        printf(" sur la fronti%cre gauche ext%crieur/zone 4.\n", 138, 130);
+
+                    else if (find_y == yb)
+                        printf(" %c l'intersection des zones 4, 7 & l'ext%crieur.\n", 133, 130);
+
+                    else if (find_y < yb + height)
+                        printf(" sur la fronti%cre gauche ext%crieur/zone 7.\n", 138, 130);
+
+                    else
+                        printf(" au coin inf%crieur gauche de la zone 7.\n", 130);
+                }
+
+                // Première colonne
+                else if (find_x < xa)
+                {
+                    if (find_y == 0)
+                        printf(" sur la fronti%cre sup%crieure ext%crieur/zone 1.\n", 138, 130, 130);
+                    
+                    else if (find_y < ya)
+                        printf(" dans la zone 1.\n");
+
+                    else if (find_y == ya)
+                        printf(" sur la fronti%cre zones 1/4.\n", 138);
+
+                    else if (find_y < yb)
+                        printf(" dans la zone 4.\n");
+
+                    else if (find_y == yb)
+                        printf(" sur la fronti%cre zones 4/7.\n", 138);
+                        
+                    else if (find_y < yb + height)
+                        printf(" dans la zone 7.\n");
+                    
+                    else
+                        printf(" sur la fronti%cre inf%crieure ext%crieur/zone 7.\n", 138, 130, 130);
+                }
+
+                // Frontière colonnes 1/2
+                else if (find_x == xa)
+                {
+                    if (find_y == 0)
+                        printf(" %c l'intersection des zones 1, 2 et l'ext%crieur.\n", 133, 130);
+                    
+                    else if (find_y < ya)
+                        printf(" sur la fronti%cre zones 1/2.\n", 138);
+
+                    else if (find_y == ya)
+                        printf(" %c l'intersection des zones 1, 2, 4 & 5.\n", 133);
+
+                    else if (find_y < yb)
+                        printf(" sur la fronti%cre zones 4/5.\n", 138);
+
+                    else if (find_y == yb)
+                        printf(" %c l'intersection des zones 4, 5, 7 & 8.\n", 133);
+
+                    else if (find_y < yb + height)
+                        printf(" sur la fronti%cre zones 7/8.\n", 138);
+
+                    else
+                        printf(" %c l'intersection des zones 7, 8 et l'ext%crieur.\n", 133, 130);
+                }
+
+                // Deuxième colonne
+                else if (find_x < xb)
+                {
+                    if (find_y == 0)
+                        printf(" sur la fronti%cre sup%crieure ext%crieur/zone 2.\n", 138, 130, 130);
+                    
+                    else if (find_y < ya)
+                        printf(" dans la zone 2.\n");
+                    
+                    else if (find_y == ya)
+                        printf(" sur la fronti%cre zones 2/5.\n", 138);
+
+                    else if (find_y < yb)
+                        printf(" dans la zone 5.\n");
+
+                    else if (find_y == yb)
+                        printf(" sur la fronti%cre zones 5/8.\n", 138);
+
+                    else if (find_y < yb + height)
+                        printf(" dans la zone 8.\n");
+                    
+                    else
+                        printf(" sur la fronti%cre inf%crieure ext%crieur/zone 8.\n", 138, 130, 130);
+                }
+
+                // Frontière colonnes 2/3
+                else if (find_x == xb)
+                {
+                    if (find_y == 0)
+                        printf(" %c l'intersection des zones 2, 3 et l'ext%crieur.\n", 133, 130);
+                    
+                    else if (find_y < yb)
+                        printf(" sur la fronti%cre zones 2/3.\n", 138);
+
+                    else if (find_y == yb)
+                        printf(" %c l'intersection des zones 2, 3, 5 & 6.\n", 133);
+
+                    else if (find_y < yb)
+                        printf(" sur la fronti%cre zones 5/6.\n", 138);
+
+                    else if (find_y == yb)
+                        printf(" %c l'intersection des zones 5, 6, 8 & 9.\n", 133);
+
+                    else if (find_y < yb + height)
+                        printf(" sur la fronti%cre zones 8/9.\n", 138);
+
+                    else
+                        printf(" %c l'intersection des zones 8, 9 et l'ext%crieur.\n", 133, 130);
+                }
+
+                // Troisième colonne
+                else if (find_x < xb + width)
+                {
+                    if (find_y == 0)
+                        printf(" sur la fronti%cre sup%crieure ext%crieur/zone 3.\n", 138, 130, 130);
+                    
+                    else if (find_y < ya)
+                        printf(" dans la zone 3.\n");
+                    
+                    else if (find_y == ya)
+                        printf(" sur la fronti%cre zones 3/6.\n", 138);
+
+                    else if (find_y < yb)
+                        printf(" dans la zone 6.\n");
+
+                    else if (find_y == yb)
+                        printf(" sur la fronti%cre zones 6/9.\n", 138);
+                        
+                    else if (find_y < yb + height)
+                        printf(" dans la zone 9.\n");
+                    
+                    else
+                        printf(" sur la fronti%cre inf%crieure ext%crieur/zone 9.\n", 138, 130, 130);
+                }
+
+                // Frontière colonnes 3/extérieur
+                if (find_x == xb + width)
+                {
+                    if (find_y == 0)
+                        printf(" au coin sup%crieur droit de la zone 3.\n", 130);
+                    
+                    else if (find_y < ya)
+                        printf(" sur la fronti%cre droite ex%crieur/zone 3.\n", 138, 130);
+
+                    else if (find_y == ya)
+                        printf(" %c l'intersection des zones 3, 6 & l'ext%crieur.\n", 133, 130);
+
+                    else if (find_y < yb)
+                        printf(" sur la fronti%cre droite ext%crieur/zone 6.\n", 138, 130);
+
+                    else if (find_y == yb)
+                        printf(" %c l'intersection des zones 6, 9 & l'ext%crieur.\n", 133, 130);
+
+                    else if (find_y < yb + height)
+                        printf(" sur la fronti%cre droite ext%crieur/zone 9.\n", 138, 130);
+
+                    else
+                        printf(" au coin inf%crieur droit de la zone 9.\n", 130);
+                }
                 break;
-        case 2:
-            running=0;
-            break;
-        
-        default:
-            printf("Mauvaise entrée. Taper un chiffre uniquement entre 1 et 2.\n");
-            break;
+
+            case 2:
+                running = 0;
+                break;
         }
     } while (running);
 
